2797250c5513ba0ea4c65e3c217cd92ad12e7b21
Eva Yonin <eva.yonin@gmail.com>
2025-07-15 19:02:07 +0300

    okhttp and unirest (with jason) for LabyrinthApi

--- src/demo/src/main/java/com/example/LabyrinthApi.java
+++ src/demo/src/main/java/com/example/LabyrinthApi.java
@@ -2,10 +2,15 @@ package com.example;
 
 import kong.unirest.Unirest; // 3.14.5 גרסה
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import kong.unirest.HttpResponse;
+//import kong.unirest.HttpResponse;
 import kong.unirest.UnirestException;
 
 //import java.net.http.HttpResponse; // 1.4.9
@@ -16,8 +21,9 @@ import kong.unirest.UnirestException;
 // זאת ספרייה שמאפשרת לשלוח בקשות HTTP
 // בצורה פשוטה, מבלי להתעסק יותר מדי בפרטים הטכניים כמו פתיחת חיבורים, קריאת תגובות ועוד.
 
-public class Main {
+public class LabyrinthApi {
     public static void main(String[] args) {
+        // UNIREST:
         try {
             HttpResponse<String> response = Unirest.get("https://app.seker.live/fm1/get-points").asString();
             // System.out.println(response.getBody()); // רואים שזה מערך כשמדפיסים את זה
@@ -38,6 +44,35 @@ public class Main {
             throw new RuntimeException();
         }
 
+        /////////////////////////////////////////////////
+        // OKHTTP :
+        try {
+            String url = "https://app.seker.live/fm1/get-points";
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> Response = client.send(
+                    request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            String body = Response.body();
+
+            JSONArray jsonArray = new JSONArray(body);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i); // point
+                int x = jsonObject.getInt("x"); // לוקח את הערך אינטג׳ר עם המפתח שהוא המחרוזת ״איקס״
+                int y = jsonObject.getInt("y");
+            }
+        } catch (UnirestException e) {
+            throw new RuntimeException();
+        }
+
+        /////////////////////////////////////////////////
         // תזכורת:
         // בקשת גט - אני מבקשת מהשרת מידע
         // בקשת פוסט - אני שןלחת לשרת מידע כדי שיעשה עם זה משהו
@@ -52,5 +87,6 @@ public class Main {
         // במקרה כאן עם האיי פי איי הזה אפשר לשלוח טווח רק של בין 5 עד 100 ואם לא שולחים
         // אז ברירת המחדל היא 30*30.
         //
+
     }
 }
